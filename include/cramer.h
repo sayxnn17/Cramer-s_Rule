5d55d664a974f55fdd76814e159635904f795734
Indrajit Bag <ibag7764@gmail.com>
2026-02-11 16:28:23 +0530

    cramer.h updated successfully

---
+++ include/cramer.h
@@ -0,0 +1,9 @@
+#ifndef CRAMER_H
+#define CRAMER_H
+
+void c_sequential(double **A,
+                             double *b,
+                             double *x,
+                             int n);
+
+#endif
